d6555a7bfd89e1c5877df47625f3eef5903eccd9
chobolevel <rodaka123@naver.com>
2025-01-13 11:16:23 +0900

    Feat: init project

---
+++ src/pages/_document.tsx
@@ -0,0 +1,17 @@
+import {Head, Html, Main, NextScript} from "next/document";
+
+const Document = () => {
+  return (
+    <Html lang="ko" >
+      <Head>
+        <link rel="icon" href="/images/favicon.ico"/>
+      </Head>
+      <body>
+        <Main/>
+        <NextScript/>
+      </body>
+    </Html>
+  )
+}
+
+export default Document
---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+const Home = () => {
+	return (
+		<>
+			<Head>
+				<meta name={'title'} content={'홈 - 초로'} />
+
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+			</Head>
+			123
+		</>
+	)
+}
+
+export default Home
